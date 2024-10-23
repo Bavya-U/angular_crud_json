ac82acaabd7016e8107b0f4fa4e5bc60f47690bf
Bavya-U <135809366+Bavya-U@users.noreply.github.com>
2024-10-23 21:11:13 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,5 +9,37 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular_crud';
+@ViewChild('myModal') model:ElementRef | undefined
+
+  openModal() {
+    const modal = document.getElementById("myModal");
+    if (modal != null) {
+      modal.style.display = 'block';
+    }
+  }
+  closeModel() {
+    if (this.model != null){
+      this.model.nativeElement.style.display = 'none';
+    }
+  }
+}
+export class Student{
+  name: string;
+  mobileNo: string;
+  email: string;
+  city: string;
+  state: string;
+  pincode: string;
+  address: string;
+
+  constructor() {
+    name= string;
+    mobileNo= string;
+    email= string;
+    city= string;
+    state= string;
+    pincode= string;
+    address= string;
+  
+  }
 }
